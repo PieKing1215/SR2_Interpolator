b7539f6d7853ef9d5238eca6a9c2a65092b61a40
PieKing1215 <PieKing1215@users.noreply.github.com>
2025-11-13 00:54:01 -0500

    Initial commit, working

---
+++ InterpolatorMod.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using MelonLoader;
+using HarmonyLib;
+using Il2CppMonomiPark.KFC.FirstPerson.SpringEffect;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SR2Interpolation
+{
+    public class InterpolatorMod : MelonMod
+    {
+        private static MelonLogger.Instance? _logger;
+
+        public override void OnInitializeMelon()
+        {
+            _logger = LoggerInstance;
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName != "PlayerCore")
+                return;
+
+            var objs = Object.FindObjectsOfType<ObjectSpringEffect>();
+            foreach (var spring in objs)
+            {
+                // the first person bobbing is controlled by a script which updates (I assume in FixedUpdate) the setpoint for a spring
+                // but by default the _followingSharpness is 10000 which is way too high and causes it to snap each update
+                spring._followingSharpness = 50f;
+                _logger?.Msg($"Adjusted ObjectSpringEffect._followingSharpness {sceneName}");
+            }
+        }
+
+        [SuppressMessage("ReSharper", "UnusedMember.Local")]
+        [HarmonyPatch(typeof(Object))]
+        private class InstantiatePatch
+        {
+            private static IEnumerable<MethodBase> TargetMethods()
+            {
+                return typeof(Object).GetMethods()
+                    .Where(method => method is { Name: "Instantiate", ContainsGenericParameters: false });
+            }
+
+            // ReSharper disable once InconsistentNaming
+            private static void Postfix(Object __result)
+            {
+                // _logger?.Msg($"Instantiate {__result} {__result.name}");
+                var go = __result.TryCast<GameObject>();
+                if (go)
+                {
+                    HandleGameObject(go!);
+                }
+                else
+                {
+                    var cmp = __result.TryCast<Component>();
+                    if (cmp)
+                    {
+                        HandleGameObject(cmp!.gameObject);
+                    }
+                }
+            }
+
+            private static void HandleGameObject(GameObject go)
+            {
+                // var count = 0;
+                foreach (var rb in go.GetComponentsInChildren<Rigidbody>(true))
+                {
+                    if (!rb.isKinematic && rb.interpolation != RigidbodyInterpolation.Interpolate)
+                    {
+                        // seems like they forgot to turn on interpolation for everything, which is why all physics objects look low fps
+                        rb.interpolation = RigidbodyInterpolation.Interpolate;
+                        // count++;
+                    }
+                }
+                // if (count > 0)
+                //     _logger?.Msg($"Added interpolation to {count} Rigidbodies");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using MelonLoader;
+using SR2Interpolation;
+
+[assembly: MelonInfo(typeof(InterpolatorMod), "Interpolator", "1.0.0", "PieKing1215")]
+[assembly: MelonGame("MonomiPark", "SlimeRancher2")]
